504f90bc92422d53b1be6b1d89b242d09dc55ff3
tuenut <root@localhost.localdomain>
2016-09-19 20:02:35 +0300

    testing for python cli-srv apps

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # mytestcode
 for learn how to use github and store some trash code
+also for some samples
---
+++ python/exp_cli-srv_cli.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import socket
+
+TCP_IP = '127.0.0.1'
+TCP_PORT = 5005
+BUFFER_SIZE = 1024
+MESSAGE = "Hello!"
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((TCP_IP, TCP_PORT))
+s.send(MESSAGE)
+data = s.recv(BUFFER_SIZE)
+
+print "recived data:", data
+
+s.send("woop!")
+
+print "recieved data:", data
+
+s.close()
+
