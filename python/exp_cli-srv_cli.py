cb92f717b6df3a7c3252081d2a7112eece3d6ff1
tuenut <netyen@yandex.ru>
2016-09-27 08:30:26 +0300

    I continue experiments with python socket

--- python/exp_cli-srv_cli.py
+++ python/exp_cli-srv_cli.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import socket
+from sys import stdin
 
 TCP_IP = '127.0.0.1'
 TCP_PORT = 5005
@@ -9,14 +10,15 @@ MESSAGE = "Hello!"
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
-s.send(MESSAGE)
-data = s.recv(BUFFER_SIZE)
 
-print "recived data:", data
-
-s.send("woop!")
-
-print "recieved data:", data
+while 1:
+    MESSAGE = stdin.readline()
+    if MESSAGE == 'exit\n' or MESSAGE == '\n':
+        break
+    #print MESSAGE
+    s.send(MESSAGE)
+    data = s.recv(BUFFER_SIZE)
+    print "recived data:", data
 
 s.close()
 
--- python/exp_cli-srv_srv.py
+++ python/exp_cli-srv_srv.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import socket
+from datetime import datetime, date, time
 
 TCP_IP = '127.0.0.1'
 TCP_PORT = 5005
@@ -10,14 +11,32 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
 s.listen(1)
 
+ledValue = {0:'-', 2:'+', 5:'+', 6:'-', 22:'+', 23:'='}
+
+print 'waiting for connection...'
+
 conn, addr = s.accept()
-print 'Connection address:', addr
 
 while 1:
     data = conn.recv(BUFFER_SIZE)
-    if not data: break
-    print 'recieved data:', data
-    data = "u say: " + data
-    conn.send(data)
+    if not data: 
+        print 'Client was disconnected.'
+        break
+    if data == '0\n':
+        timeNow = datetime.now()
+        try:
+            if ledValue[timeNow.hour] == '+':
+                ans = round( ( timeNow.minute / 60.0 ) * 255 )
+            elif ledValue[timeNow.hour] == '=':
+                ans = 255
+            elif ledValue[timeNow.hour] == '-':
+                ans = round( ( 255 - ( timeNow.minute / 60.0 ) * 255 ) )
+        except KeyError:
+            ans = 0
+    else:
+        ans = 'You say: ' + data + 'but there is not valid request!\n'
+    print 'Client request: ', data
+    print 'Server answer: ', ans
+    conn.send(str(ans))
 conn.close()
 
