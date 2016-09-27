5e5e201b85e8d90a6836d95eff8f5e2766a6b8fa
tuenut <netyen@yandex.ru>
2016-09-27 12:03:00 +0200

    sync

--- python/exp_cli-srv_srv.py
+++ python/exp_cli-srv_srv.py
@@ -9,19 +9,12 @@ BUFFER_SIZE = 20
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
-s.listen(1)
 
 ledValue = {0:'-', 2:'+', 5:'+', 6:'-', 22:'+', 23:'='}
 
 print 'waiting for connection...'
 
-conn, addr = s.accept()
-
-while 1:
-    data = conn.recv(BUFFER_SIZE)
-    if not data: 
-        print 'Client was disconnected.'
-        break
+def dataProcessing(inData):
     if data == '0\n':
         timeNow = datetime.now()
         try:
@@ -35,8 +28,20 @@ while 1:
             ans = 0
     else:
         ans = 'You say: ' + data + 'but there is not valid request!\n'
-    print 'Client request: ', data
-    print 'Server answer: ', ans
-    conn.send(str(ans))
+
+
+while 1:
+    s.listen(1)
+    conn, addr = s.accept()
+    while 1:
+        data = conn.recv(BUFFER_SIZE)
+        if not data: 
+            print 'Client was disconnected.'
+            conn.close()
+            break
+        else:
+            print 'Client request: ', data
+            print 'Server answer: ', ans
+            conn.send(str(ans))
 conn.close()
 
